830093b62085af0ad4539095a564719b7c8c77e6
yesmola <1062349166@qq.com>
2021-08-23 23:26:47 +0800

    [feat]: update LEXER and TOKEN

---
+++ src/monkey/token/token.go
@@ -0,0 +1,72 @@
+package token
+
+type Type string
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+)
+
+// Identifiers and literals
+const (
+	IDENT = "IDENT" // identifiers add, foobar, x, y, ...
+	INT   = "INT"   // int literals
+)
+
+// Operators
+const (
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	BANG     = "!"
+	ASTERISK = "*"
+	SLASH    = "/"
+
+	LT  = "<"
+	GT  = ">"
+	EQ  = "=="
+	NEQ = "!="
+)
+
+// Delimiters
+const (
+	COMMA     = ","
+	SEMICOLON = ";"
+	LPAREN    = "("
+	RPAREN    = ")"
+	LBRACE    = "{"
+	RBRACE    = "}"
+)
+
+// Keywords
+const (
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+	TRUE     = "true"
+	FALSE    = "false"
+	IF       = "if"
+	ELSE     = "else"
+	RETURN   = "return"
+)
+
+type Token struct {
+	Type    Type
+	Literal string
+}
+
+var keywords = map[string]Type{
+	"fn":     FUNCTION,
+	"let":    LET,
+	"true":   TRUE,
+	"false":  FALSE,
+	"if":     IF,
+	"else":   ELSE,
+	"return": RETURN,
+}
+
+func LookupIdent(ident string) Type {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
